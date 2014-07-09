06fc71e1e71d4bc601b85e1f840a4b7b132e8ad6
Pierre Fayol <pierre.fayol@cifacom.com>
2014-07-09 18:41:49 +0200

    versionnig v1

--- Api-Mobile/API/index.php
+++ Api-Mobile/API/index.php
@@ -2,79 +2,79 @@
 //exit("on y est");
 
 //Redirection des classes
-require("libs/Toro.php");
-require("libs/api.php");
+require("v1/libs/Toro.php");
+require("v1/libs/api.php");
 
 //Connection à la base de données
-require("libs/mysql.php");
+require("v1/v1/libs/mysql.php");
 
 //Requete sur les users
-require("libs/users_queries.php");
+require("v1/libs/users_queries.php");
 //Requete sur un user
-require("libs/user_queries.php");
+require("v1/libs/user_queries.php");
 //Requete sur les films
-require("libs/movies_queries.php");
+require("v1/libs/movies_queries.php");
 //Requete sur un film
-require("libs/movie_queries.php");
+require("v1/libs/movie_queries.php");
 //Requete sur les genres
-require("libs/genre_queries.php");
+require("v1/libs/genre_queries.php");
 //Requete de la recherche
-require("libs/search_queries.php");
+require("v1/libs/search_queries.php");
 
 
 //Requete sur les users
-//require("libs/users_queries.php");
+//require("v1/libs/users_queries.php");
 //classes sur les users
-require("handlers/users_handler.php");
+require("v1/handlers/users_handler.php");
 //classes sur les films
-require("handlers/movies_handler.php");
+require("v1/handlers/movies_handler.php");
 //classes sur un user
-require("handlers/user_handler.php");
+require("v1/handlers/user_handler.php");
 //classes sur un film
-require("handlers/movie_handler.php");
+require("v1/handlers/movie_handler.php");
 //Classe sur les likes d'un user
-require("handlers/userlikes_handler.php");
+require("v1/handlers/userlikes_handler.php");
 //classes sur les films
-require("handlers/userdislikes_handler.php");
+require("v1/handlers/userdislikes_handler.php");
 //classes sur un user
-require("handlers/userwatched_handler.php");
+require("v1/handlers/userwatched_handler.php");
 //classes sur un film
-require("handlers/userwatchlist_handler.php");
+require("v1/handlers/userwatchlist_handler.php");
 //Classes sur les genres
-require("handlers/genre_handler.php");
+require("v1/handlers/genre_handler.php");
 //Classes recherche
-require("handlers/search_handler.php");
+require("v1/handlers/search_handler.php");
 
 ToroHook::add("404", function() {
     echo "Not found";
 });
 
 Toro::serve(array(
-    "/"                                 => "UsersHandler",
+    "/v1/"                                 => "UsersHandler",
 
     //creation de user
-    "/users"                            => "UsersHandler",              //GET, POST fait
+    "/v1/users"                            => "UsersHandler",              //GET, POST fait
 
     //affichage d'un user
-    "/users/:number"                    => "UserHandler",               //GET, PUT, DELETE fait
-    "/users/:number/likes"              => "UserLikesHandler",          //GET fait
-    "/users/:number/likes/:number"      => "UserLikesHandler",          //POST - DELETE fait
-    "/users/:number/dislikes"           => "UserDislikesHandler",       //GET fait
-    "/users/:number/dislikes/:number"   => "UserDislikesHandler",       //POST - DELETE fait
-    "/users/:number/watched"            => "UserWatchedHandler",        //GET fait
-    "/users/:number/watched/:number"    => "UserWatchedHandler",        //POST - DELETE fait
-    "/users/:number/watchlist"          => "UserWatchlistHandler",      //GET fait
-    "/users/:number/watchlist/:number"  => "UserWatchlistHandler",      //POST - DELETE fait
+    "/v1/users/:number"                    => "UserHandler",               //GET, PUT, DELETE fait
+    "/v1/users/:number/likes"              => "UserLikesHandler",          //GET fait
+    "/v1/users/:number/likes/:number"      => "UserLikesHandler",          //POST - DELETE fait
+    "/v1/users/:number/dislikes"           => "UserDislikesHandler",       //GET fait
+    "/v1/users/:number/dislikes/:number"   => "UserDislikesHandler",       //POST - DELETE fait
+    "/v1/users/:number/watched"            => "UserWatchedHandler",        //GET fait
+    "/v1/users/:number/watched/:number"    => "UserWatchedHandler",        //POST - DELETE fait
+    "/v1/users/:number/watchlist"          => "UserWatchlistHandler",      //GET fait
+    "/v1/users/:number/watchlist/:number"  => "UserWatchlistHandler",      //POST - DELETE fait
 
-    "/users/:number/followed/"          => "FollowedHandler",           //GET fait
-    "/users/:number/followed/:number"   => "FollowedHandler",           //POST, DELETE fait
+    "/v1/users/:number/followed/"          => "FollowedHandler",           //GET fait
+    "/v1/users/:number/followed/:number"   => "FollowedHandler",           //POST, DELETE fait
 
-    "/users/:number/followers/"         => "FollowersHandler",          //GET fait
+    "/v1/users/:number/followers/"         => "FollowersHandler",          //GET fait
 
-    "/movies"                           => "MoviesHandler",             //GET, POST fait
-    "/movies/:number"                   => "MovieHandler",              //GET, PUT, DELETE fait
+    "/v1/movies"                           => "MoviesHandler",             //GET, POST fait
+    "/v1/movies/:number"                   => "MovieHandler",              //GET, PUT, DELETE fait
 
-    "/search"                           => "SearchHandler",             //GET fait
+    "/v1/search"                           => "SearchHandler",             //GET fait
 
-    "/genres"                           => "GenreHandler"               //GET fait
+    "/v1/genres"                           => "GenreHandler"               //GET fait
 ));
